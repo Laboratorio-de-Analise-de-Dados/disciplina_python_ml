24c98d562a2f08246badd9988d36564e0d7373f2
Guilherme F Silveira <gfsilveira@gmail.com>
2026-07-26 17:30:43 -0300

    Clusterização aula 09 finalizada

---
+++ dev/script/aulas_teoricas/src/clustering.py
@@ -0,0 +1,318 @@
+# Métricas de avaliação de clustering
+from sklearn.metrics import silhouette_score
+from sklearn.metrics import calinski_harabasz_score
+from sklearn.metrics import davies_bouldin_score
+from sklearn.metrics import adjusted_rand_score
+from sklearn.metrics import adjusted_mutual_info_score
+
+# Bibliotecas para clustering
+from sklearn.cluster import KMeans, DBSCAN, AgglomerativeClustering
+from scipy.cluster.hierarchy import linkage
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.neighbors import kneighbors_graph
+
+# Bibliotecas para o modelo de clustering
+import numpy as np
+import pandas as pd
+
+# Geração de gráficos
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def silhouette_scorer_func(estimator, X) -> float:
+    '''
+        Função para calcular a pontuação de Silhouette para um estimador de
+        clustering.
+            Parâmetros:
+                estimator: Estimador de clustering (ex: KMeans)
+                X: Dados de entrada para o cálculo da pontuação
+            Retorna:
+                float: Pontuação de Silhouette média para os dados fornecidos
+    '''
+    labels = estimator.fit_predict(X)
+    # Silhouette precisa de pelo menos 2 clusters distintos no conjunto de validação
+    if len(np.unique(labels)) > 1:
+        return silhouette_score(X, labels)
+    return -1.0  # Retorna pontuação baixa caso haja falha de separação no fold
+
+
+class Clustering:
+
+    def __init__(
+            self,
+            c1: str,
+            c2: str,
+            df: pd.DataFrame,
+            modelo: str) -> None:
+        self.c1 = c1
+        self.c2 = c2
+        self.df = df
+        self.modelo = modelo
+
+        return None
+
+    def __calcular_metricas(
+            self,
+            X_scaled: np.ndarray,
+            pred: np.ndarray,
+            obs: np.ndarray) -> tuple:
+        '''
+            Função para calcular métricas de avaliação de clustering.
+                Parâmetros:
+                    X_scaled: np.array - Dados escalonados utilizados para o clustering
+                    pred: np.array - Rótulos previstos pelo modelo de clustering
+                    obs: np.array - Rótulos observados (verdadeiros) para comparação
+                Retorna:
+                    None - Apenas imprime as métricas calculadas
+        '''
+        ss = np.nan
+        chs = np.nan
+        dbs = np.nan
+        ari = np.nan
+        ami = np.nan
+
+        try:
+            # Calcula a métrica de Silhouette 
+            ss = round(silhouette_score(X_scaled, pred), 3)
+            # Calcula a métrica de Calinski-Harabasz
+            chs = round(calinski_harabasz_score(X_scaled, pred), 3)
+
+            # Calcula a métrica de Davies-Bouldin
+            dbs = round(davies_bouldin_score(X_scaled, pred), 3)
+
+            # Calcula a métrica de Adjusted Rand Score
+            ari = round(adjusted_rand_score(obs, pred), 3)
+
+            # Calcula a métrica de Adjusted Mutual Info Score
+            ami = round(adjusted_mutual_info_score(obs, pred), 3)
+        except Exception as e:
+            pass
+
+        return (ss, chs, dbs, ari, ami)
+
+
+    def __grafico_scatter(
+            self,
+            hue: str | list | np.ndarray) -> None:
+        '''
+            Função para gerar um gráfico de dispersão (scatter plot) com base em
+            duas variáveis e uma variável de agrupamento (hue).
+                Parâmetros:
+                    hue: str|list|np.array - Nome da coluna do DataFrame ou array
+                                                contendo os valores para colorir os
+                                                pontos no gráfico.
+                Retorna:
+                    None - Apenas exibe o gráfico gerado.
+        '''
+        # Gráfico de correlação neutra entre os dados
+        p = sns.jointplot(
+            x=self.c1,
+            y=self.c2,
+            hue=hue,
+            height=6,
+            ratio=4,
+            marginal_ticks=True,
+            data=self.df,
+        )
+        p.fig.suptitle(f"Correlação Variáveis Discretas", y=1.02)
+        p.set_axis_labels(
+            xlabel=self.c1,
+            ylabel=self.c2
+        )
+        return None
+
+    def __grafico_box(
+            self,
+            cluster_labels: np.ndarray) -> None:
+        fig, ax = plt.subplots(1, 2, figsize=(13, 5))
+        plt.suptitle('Diferença das classes clusterizadas')
+        g = sns.boxplot(
+            x=cluster_labels,
+            y=self.c1,
+            data=self.df,
+            ax=ax[0]
+        )
+        g.set(
+            title = f' {self.c1} para Clusters',
+            # xlabel = cluster_labels,
+            ylabel = self.c1,
+        )
+        g = sns.boxplot(
+            x=cluster_labels,
+            y=self.c2,
+            data=self.df,
+            ax=ax[1]
+        )
+        g.set(
+            title = f' {self.c2} para Clusters',
+            # xlabel = cluster_labels,
+            ylabel=self.c2,
+        )
+
+        return None
+
+    def __param_grid(self) -> dict:
+        if self.modelo == 'kmeans':
+            retorno = {
+                        "n_clusters": [2, 3, 4, 5, 6],
+                        "init": ["k-means++", "random"],
+                        "n_init": ['auto', 10, 20, 30],
+                    }
+        elif self.modelo == 'dbscan':
+            retorno = {
+                        "eps": [0.1, 0.2, 0.3, 0.4, 0.5],
+                        "min_samples": [3, 5, 10, 15],
+                        "metric": ["euclidean", "manhattan"],
+                    }
+        elif self.modelo == 'agglomerative':
+            retorno = {
+                        "n_clusters": [2, 3, 4, 5, 6],
+                        "metric": ['euclidean'],
+                        "linkage": ["ward", "complete", "average", "single"],
+                    }
+        return retorno
+
+    def cross_validation(
+            self,
+            n_splits: int = 5,
+            random_state: int = 42,
+            print_metricas: bool = True
+        ) -> None | np.ndarray | float:
+        '''
+            Função para realizar validação cruzada utilizando a pontuação de 
+            Silhouette para um par de variáveis em um DataFrame.
+                Parâmetros:
+                    self.c1: str - Nome da primeira variável
+                    self.c2: str - Nome da segunda variável
+                    self.df: pd.DataFrame - DataFrame contendo as variáveis
+                    n_splits: int - Número de folds para a validação cruzada.
+                                    Default é 5.
+                    random_state: int - Semente para reprodução dos resultados.
+                                    Default é 42.
+                    print_metricas: bool - Se True, imprime as métricas de
+                                        avaliação do clustering. Default é True.
+                Retorna:
+                    None
+        '''
+        # Preparação dos Dados Sintéticos
+        X_raw = self.df[[self.c1,self.c2]]
+
+        scaler = StandardScaler()
+        X_scaled = scaler.fit_transform(X_raw)
+
+        # Grid Search Manual integrando cross_val_score
+
+        kf = KFold(
+            n_splits=n_splits,
+            shuffle=True,
+            random_state=random_state
+        )
+
+        best_score = -1.0
+        best_params = {}
+        results = []
+
+        param_grid = self.__param_grid()
+        param_grid_keys = list(param_grid.keys())
+        # Iteração sobre a grade de hiperparâmetros
+        for a in param_grid[param_grid_keys[0]]:
+            for b in param_grid[param_grid_keys[1]]:
+                for c in param_grid[param_grid_keys[2]]:
+
+                    if self.modelo == 'kmeans':
+                        # Instância do modelo com a combinação atual
+                        model = KMeans(
+                            n_clusters=a,
+                            init=b,
+                            n_init=c,
+                            random_state=random_state,
+                        )
+                    elif self.modelo == 'dbscan':
+                        # Instância do modelo com a combinação atual
+                        model = DBSCAN(
+                            eps=a,
+                            min_samples=b,
+                            metric=c,
+                            n_jobs=-1
+                        )
+                    elif self.modelo == 'agglomerative':
+                        model = AgglomerativeClustering(
+                            n_clusters=a,
+                            metric=b,
+                            linkage=c,
+                            # connectivity=kneighbors_graph(self.df[[self.c1,self.c2]], n_neighbors=5, include_self=False),
+                        )
+                    # Execução do cross_val_score do scikit-learn
+                    scores = cross_val_score(
+                        estimator=model,
+                        X=X_scaled,
+                        cv=kf,
+                        # Usando a função scorer
+                        scoring=silhouette_scorer_func,
+                        n_jobs=-1,  # Paralelização dos folds
+                    )
+
+                    mean_score = np.mean(scores)
+                    std_score = np.std(scores)
+
+                    results.append(
+                        {
+                            param_grid_keys[0]: a,
+                            param_grid_keys[1]: b,
+                            param_grid_keys[2]: c,
+                            "mean_score": mean_score,
+                            "std_score": std_score,
+                        }
+                    )
+
+                    if mean_score > best_score:
+                        best_score = mean_score
+                        best_params = {
+                            param_grid_keys[0]: a,
+                            param_grid_keys[1]: b,
+                            param_grid_keys[2]: c,
+                        }
+
+        # Treinamento do modelo final no dataset completo com os melhores
+        # hiperparâmetros
+        if self.modelo == 'kmeans':
+            best_model = KMeans(**best_params, random_state=random_state)
+        elif self.modelo == 'dbscan':
+            best_model = DBSCAN(**best_params, n_jobs=-1)
+        elif self.modelo == 'agglomerative':
+            best_model = AgglomerativeClustering(**best_params)
+                                                #  , connectivity=kneighbors_graph(self.df[[self.c1,self.c2]], n_neighbors=5, include_self=False))
+
+        cluster_labels = best_model.fit_predict(X_scaled)
+
+        # Mostra as métricas de avaliação do clustering
+        metricas = {
+            'X_scaled': X_scaled,
+            'pred': cluster_labels,
+            'obs': np.array(self.df['classe'].values)
+        }
+        if print_metricas:
+            # Resultados e Ajuste Final
+            print("=== MELHOR CONFIGURAÇÃO ENCONTRADA VIA cross_val_score ===")
+            print(f"Parâmetros: {best_params}")
+            print(f"Silhouette Score Médio (CV): {best_score:.4f}\n")
+
+            # Calcular métricas de avaliação do clustering com os melhores 
+            # hiperparâmetros
+            
+            ss, chs, dbs, ari, ami = self.__calcular_metricas(**metricas)
+            print(f"Silhouette Score -> {ss}")
+            print(f"Calinski Harabasz Score -> {chs}")
+            print(f"Davies Bouldin Score -> {dbs}")
+            print(f"Adjusted Rand Score -> {ari}")
+            print(f"Adjusted Mutual Info Score -> {ami}")
+
+            # Graficos de relação entre as variáveis e os clusters
+            self.__grafico_scatter(hue='classe')
+            self.__grafico_scatter(hue=cluster_labels)
+            self.__grafico_box(cluster_labels=cluster_labels)
+            plt.show()
+
+            return None
+        return cluster_labels, self.__calcular_metricas(**metricas)
