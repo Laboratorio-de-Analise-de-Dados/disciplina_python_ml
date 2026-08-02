69d51dbabf660ef87aedf91443c66daec0c40300
Guilherme F Silveira <gfsilveira@gmail.com>
2026-08-02 18:35:30 -0300

    Atualizando pdfs

---
+++ dev/script/app_aula/main.py
@@ -0,0 +1,13 @@
+def pagina_inicial() -> str:
+    '''
+        Função de inicialização da página inicial.
+        Parametros:
+            None
+        Retorna:
+            str: Mensagem de sucesso.
+    '''
+    return "Página inicial carregada com sucesso."
+
+if __name__ == "__main__":
+    resultado = pagina_inicial()
+    print(resultado)
